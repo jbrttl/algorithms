9916fd3ce9ade18fdddf8249f90c05da9058b60f
JB <jakub.brettl@gmail.com>
2022-01-08 15:53:12 +0100

    Implementing stacks

---
+++ stacks/stack.py
@@ -0,0 +1,18 @@
+class Stack(object):
+    
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+class StackOverflow(Exception):
+    pass
+
+class StackUnderflow(Exception):
+    pass
+
+
+if __name__ == '__main__':
+    stack = Stack()
+    print(stack)
