9586afa77b9cc055852282421a93dfff36e4e90b
JB <jakub.brettl@gmail.com>
2022-01-17 20:44:51 +0100

    Speeding up NPV calculations

--- investing/investing.py
+++ investing/investing.py
@@ -17,17 +17,21 @@ class NPV:
         self.years = years
         self.outflow = outflow
 
-    def _get_PV(self):
+    def _get_DCF(self):
         """Helper method to calculate PV of cashflows."""
         years_array = np.arange(1, self.years+1)
         discount_factor = (1+self.drate)**(years_array)
         return np.round_(self.cashflow/discount_factor)
 
     def calc_NPV(self):
-        pv = self._get_PV()
-        return np.sum(pv-self.outflow)
+        dcf = self._get_DCF()
+        return np.sum(dcf-self.outflow)
+
+    def calc_PV(self):
+        return np.sum(self._get_DCF())
 
 if __name__ == '__main__':
     npv = NPV(0.1,10000.,0.,10)
-    print(npv._get_PV())
+    print(npv._get_DCF())
+    print(npv.calc_PV())
     print(npv.calc_NPV())
