93a394f27980a16b3dc6a3521eafce730f37e69c
JB <jakub.brettl@gmail.com>
2022-02-04 21:42:33 +0100

    Restructure option pricing code to: 1/ Brownian motion only, 2/European call, and 3/ Up and out Barrier Option

--- financial-math/europeanCallOption.py
+++ financial-math/europeanCallOption.py
@@ -30,6 +30,17 @@ class BrownianMotion:
             self.prices.append(self.current_price)
             self.T -= self.dt # Account for the step in time
 
+class EuropeanCallPayoff:
+
+    def __init__(self, strike):
+        self.strike = strike
+
+    def get_payoff(self,stock_price):
+        if stock_price > self.strike:
+            return stock_price - self.strike
+        else:
+            return 0
+
 
 if __name__ == '__main__':
 
@@ -44,6 +55,18 @@ if __name__ == '__main__':
     for i in range(0, paths):
         price_paths.append(BrownianMotion(initial_price, drift, volatility, dt, T).prices)
 
+
+    # European call
+    call_payoffs = []
+    ec = EuropeanCallPayoff(100)
+    risk_free_rate = .01
+
+    for price_path in price_paths:
+        call_payoffs.append(ec.get_payoff(price_path[-1])/(1+risk_free_rate))
+
     for price_path in price_paths:
         plt.plot(price_path)
+
     plt.show()
+
+    print(np.average(call_payoffs)*100)
--- financial-math/upAndOutBarrierOption.py
+++ financial-math/upAndOutBarrierOption.py
@@ -31,6 +31,26 @@ class BrownianMotion:
             self.T -= self.dt # Account for the step in time
 
 
+class UpandOutBarrierOption:
+    def __init__(self,strike,barrier_level):
+        self.strike = strike
+        self.barrier_level = barrier_level
+        self.barrier_trigger = False
+
+    def check_barrier(self,stock_price):
+        if stock_price>self.barrier_level:
+            self.barrier_level = True
+
+    def get_payoff(self,stock_price):
+        if not self.barrier_trigger:
+            if stock_price>self.strike:
+                return stock_price - self.strike
+            else:
+                return 0
+        else:
+            return 0
+
+
 if __name__ == '__main__':
 
     paths = 100
@@ -44,6 +64,17 @@ if __name__ == '__main__':
     for i in range(0, paths):
         price_paths.append(BrownianMotion(initial_price, drift, volatility, dt, T).prices)
 
+    # Up and Out barrier option
+
+    barrier_payoff = list()
+    risk_free_rate = 0.01
+    for price_path in price_paths:
+        bc = UpandOutBarrierOption(100,100.5)
+        for price in price_path:
+            bc.check_barrier(price)
+        barrier_payoff.append(bc.get_payoff(price_path[-1])/(1+risk_free_rate))
+
     for price_path in price_paths:
         plt.plot(price_path)
     plt.show()
+    print(np.average(barrier_payoff)*100)
